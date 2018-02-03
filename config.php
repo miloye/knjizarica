c0610b92fc4c3ac96a760f6c835b0dc919982541
stefanbozovic <34029896+stefanbozovic@users.noreply.github.com>
2018-02-03 10:11:06 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/novi/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/novi/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/novi/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/novi/upload/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/novi/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/novi/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/novi/upload/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/novi/upload/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/novi/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/novi/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/novi/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/novi/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/novi/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'proba3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
